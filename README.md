50a7425935bfb1319d665abb0c95c6e4f524a537
rickrribeiro <rickribeiro99@gmail.com>
2021-10-25 14:59:50 -0300

    finalizando projeto de testes

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Using KafkaJS and Socket.io
 ### 2. Create and configure .env - see .env.example
 ### 3. Install deps
 ### 4. node index.js 
-### 5. use kafkajs folder scripts if want to test a specific function, kafka-node is not working (maybe its working now and i forgort to update the readme) 
+### 5. use checkbox to change user
+### 6. use kafkajs folder scripts if want to test a specific function, kafka-node is not working (maybe its working now and i forgort to update the readme) 
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,27 +1,71 @@
 document.getElementById("messagesCount").innerHTML="1000 messagens"
 const socket = io();
 socket.on('message', function(msg) {
+    const sender = JSON.parse(msg).sender
     console.log(msg)
-    str= `
-    <div class="d-flex justify-content-end mb-4">
-    <div class="msg_cotainer_send">
-    ${JSON.parse(msg).message}
-    <span class="msg_time_send">8:55 AM, Today</span>
-    </div>
-    <div class="img_cont_msg">
-    <img src="img/ricardo.jpg" class="rounded-circle user_img_msg">
-    </div>
-    </div>
-    `
+    if(sender.toLowerCase()=="ricardo"){
+
+        str= `
+        <div class="d-flex justify-content-end mb-4">
+        <div class="msg_cotainer_send">
+        ${JSON.parse(msg).message}
+        <span class="msg_time_send">${new Date().getMinutes()}</span>
+        </div>
+        <div class="img_cont_msg">
+        <img src="img/ricardo.jpg" class="rounded-circle user_img_msg">
+        </div>
+        </div>
+        `
+    }else{
+        str = `
+        <div class="d-flex justify-content-start mb-4">
+            <div class="img_cont_msg">
+                <img src="img/thales.jpeg" class="rounded-circle user_img_msg">
+            </div>
+            <div class="msg_cotainer">
+               ${JSON.parse(msg).message}
+                <span class="msg_time">${new Date().getMinutes()}</span>
+            </div>
+        </div>
+        `
+    }
     document.getElementById("msgThalesRicardo").innerHTML+=str
 });
 
-
+document.getElementById("isThales").addEventListener('change', function() {
+    if (this.checked) {
+        document.getElementById("userInfo").innerHTML=`
+        <div class="img_cont">
+            <img src="img/ricardo.jpg" class="rounded-circle user_img">
+             <span class="online_icon"></span>
+        </div>
+        <div class="user_info">
+            <span>Chat with Ricardo</span>
+            <p id="messagesCount" ></p>
+        </div>
+        `
+    } else {
+        document.getElementById("userInfo").innerHTML=`
+        <div class="img_cont">
+            <img src="img/thales.jpeg" class="rounded-circle user_img">
+             <span class="online_icon"></span>
+        </div>
+        <div class="user_info">
+            <span>Chat with Thales</span>
+            <p id="messagesCount" ></p>
+        </div>
+        `
+    }
+  });
 
 function sendMessage(){
     const message = document.getElementById("textMessage").value
+    const sender = document.getElementById("isThales").checked?"thales":"ricardo";
+
   
+    
     if(message!= undefined && message.length>0){
-        socket.emit('sendmessage', `{"sender":"ricardo","message":"${message.replace("\n"," ")}"}`);
+        socket.emit('sendmessage', `{"sender":"${sender}","message":"${message.replace("\n"," ")}"}`);
+        document.getElementById("textMessage").value = ""
     }
 }
